0d325d6202616354d06e238ced732e03d99a2b8a
ktrnthsnr <asakthisner@hotmail.com>
2020-05-21 01:05:33 -0700

    updated player.js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,11 +4,12 @@ jest.mock('../lib/Potion.js');
 console.log(new Potion());
 // -- run test $ nmp run test Player
 
-
+//-- import the Player() constructor
 const Player = require('../lib/Player');
 
+// -- first test, by creating new player object
 test('creates a player object', () => {
-    // -- test creates a new player object 'Dave'
+    // -- new player object 'Dave'
     const player = new Player('Dave');
     // -- whereby the test object has 4 properties
     expect(player.name).toBe('Dave');
@@ -21,3 +22,22 @@ test('creates a player object', () => {
     );
   });
 
+  // -- second test, for stats
+  test("gets player's stats as an object", () => {
+    // -- create a new object 'Dave'
+    const player = new Player('Dave');  
+    // -- the new object will have 4 new properties
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  // -- another test
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');  
+    // -- return an array
+    expect(player.getInventory()).toEqual(expect.any(Array));  
+    player.inventory = [];  
+    expect(player.getInventory()).toEqual(false);
+  });
